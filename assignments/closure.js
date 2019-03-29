b0aa8a51a3258037dc18bf0e77f90f12db869b06
Matt Conway <mattconway727@gmail.com>
2019-03-28 22:52:01 -0500

    initial commit

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,9 +1,7 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
 
-
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
-
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
@@ -19,3 +17,6 @@ const counterFactory = () => {
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
 };
+
+lkasjdf;
+kasjdf;
